8a800cd769c613652006bee51f5b7e5dbb962782
Ravil <raviltchusmakaev@yandex.ru>
2023-01-27 11:31:32 +0300

    first changed

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-
+import os
+file_to_open = os.path.expanduser('cook_book.txt')
+with open('cook_book.txt', 'rt',) as f:
+    meal = {}
+    for line in f:
+        meal_name = line.strip()
+        dish_count = int(f.readline())
+        dish = []
+        for i in range(dish_count):
+            abc = f.readline().strip()
+            ingredient_name, quantity, measure = abc.split(' | ')
+            dish.append({
+                'ingredient_name': ingredient_name,
+                'quantity': quantity,
+                'measure': measure
+            })
+        f.readline()
+        meal[meal_name] = dish
+print(meal)
