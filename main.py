9312df833fa4125fa10408bd8b99c7cce62f58a2
Ravil <raviltchusmakaev@yandex.ru>
2023-01-27 19:41:47 +0300

    final changed

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ with open('cook_book.txt', 'rt',) as f:
         dish = []
         for i in range(dish_count):
             abc = f.readline()
-            ingredient_name, quantity, measure = abc.split(' | ')
+            ingredient_name, quantity, measure = abc.split(" | ")
             dish.append({'ingredient_name': ingredient_name,  'quantity': quantity, 'measure': measure})
         f.readline()
         meal[meal_name] = dish
 pprint(meal)
-
-
+def get_shop_list_by_dishes(dishes, persons=int):
+    print('Ознакомьтесь с Нашим меню :')
+    pprint(meal)
+    print()
+    shopping_list = {}
+    try:
+        for dish in dishes:
+            for item in (meal[meal_name]):
+                items_list = dict([(item['ingredient_name'], {'measure': item['measure'], 'quantity': int(item['quantity'])*persons})])
+                if shopping_list.get(item['ingredient_name']):
+                    extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) +
+                                  int(items_list[item['ingredient_name']]['quantity']))
+                    shopping_list[item['ingredient_name']]['quantity'] = extra_item
+                else:
+                    shopping_list.update(items_list)
+        print(f"Для приготовления блюд на {persons} человек  нам необходимо купить:")
+        pprint(shopping_list)
+    except KeyError:
+        print("Вы ошиблись в названии блюда, проверьте ввод")
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
 
 
