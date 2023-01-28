53b5a0b88b593ca4363705a5f944f75fdb324f69
Ravil <raviltchusmakaev@yandex.ru>
2023-01-28 22:57:20 +0300

    final changed_v2

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from pprint import pprint
-with open('cook_book.txt', 'rt',) as f:
-    meal = {}
-    for line in f:
-        meal_name = line.strip()
-        dish_count = int(f.readline())
-        dish = []
-        for i in range(dish_count):
-            abc = f.readline()
-            ingredient_name, quantity, measure = abc.split(" | ")
-            dish.append({'ingredient_name': ingredient_name,  'quantity': quantity, 'measure': measure})
-        f.readline()
-        meal[meal_name] = dish
-pprint(meal)
+def cookbook():
+    with open('cook_book.txt', 'rt',) as f:
+        meal = {}
+        for line in f:
+            dish_name = line[:-1]
+            meal_name = line.strip()
+            dish_count = int(f.readline())
+            dish = []
+            for i in range(int(dish_count)):
+                dish_items = dict.fromkeys(['ingredient_name', 'quantity', 'measure']) # временный словарь
+                ingridient = f.readline().strip().split(' | ') # перемещение по файлу
+                for item in ingridient:
+                    dish_items['ingredient_name'] = ingridient[0]
+                    dish_items['quantity'] = ingridient[1]
+                    dish_items['measure'] = ingridient[2]
+                dish.append(dish_items)
+                cook_book = {dish_name: dish}
+                meal.update(cook_book)
+            f.readline()
+    return(meal)
+pprint(cookbook())
+
 def get_shop_list_by_dishes(dishes, persons=int):
+    menu = (cookbook())
     print('Ознакомьтесь с Нашим меню :')
-    pprint(meal)
+    pprint(cookbook())
     print()
     shopping_list = {}
     try:
         for dish in dishes:
-            for item in (meal[meal_name]):
+            for item in (menu[dish]):
                 items_list = dict([(item['ingredient_name'], {'measure': item['measure'], 'quantity': int(item['quantity'])*persons})])
                 if shopping_list.get(item['ingredient_name']):
                     extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) +
