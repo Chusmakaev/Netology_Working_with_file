7ef752c8da7e5e416c3893078ba492c3410422fb
Ravil <raviltchusmakaev@yandex.ru>
2023-01-30 18:42:34 +0300

    final_v1

--- task3.py
+++ task3.py
@@ -1,5 +1,5 @@
+from pprint import pprint
 import os
-
 def create_combined_list(directory):
     files = os.listdir(directory)
     combined_list = []
@@ -10,21 +10,16 @@ def create_combined_list(directory):
             for line in cur_file:
                 combined_list[-1][2].append(line.strip())
                 combined_list[-1][1] += 1
-                # print(line)
 
     return sorted(combined_list, key=lambda x: x[2], reverse=True)
-
-
+pprint(create_combined_list('sorted'))
 def create_file_from_directory(directory, filename):
     with open(filename + '.txt', 'w+') as newfile:
         for file in create_combined_list(directory):
             newfile.write(f'File name: {file[0]}\n')
             newfile.write(f'Length: {file[1]} string(s)\n')
-            # print(file)
             for string in file[2]:
                 newfile.write(string + '\n')
-                # print(string)
             newfile.write('-------------------\n')
-
 
 print(create_file_from_directory('sorted', 'newfiletxt'))
